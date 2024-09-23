ca21fb3a0c9d873fdfe776ac363c0e2577eb3143
Saul Lopez Lucas <saullopezlucas@gmail.com>
2024-09-22 21:15:55 -0400

    added videos and redesigned UI

--- src/App.jsx
+++ src/App.jsx
@@ -9,55 +9,75 @@ const App = () => {
       description:
         "Learn to manager your dinero wisely and plan for the future.",
       tagline: "Plan today to thrive mañana.",
+      icon: "bi-wallet",
+      link: "https://www.youtube.com/watch?v=sVKQn2I4HDM",
     },
     {
       title: "Introduction to Saving",
       description: "Setting aside dinero for future needs or emergencies",
       tagline: "Every penny saved is a step toward your sueños.",
+      icon: "bi-piggy-bank",
+      link: "https://www.youtube.com/watch?v=ms1nTeFO7ps",
     },
     {
       title: "Introduction to Debt Management",
       description: "Strategies to pay down deudas and avoid new ones.",
       tagline: "Take charge of your debt, reclaim your futuro.",
+      icon: "bi-bank",
+      link: "https://www.youtube.com/watch?v=PvEUj_mRfIo",
     },
     {
       title: "Introduction to Investing",
       description:
         "Using money to buy activos with the expectation of returns.",
       tagline: "Invest today for a prosperous mañana.",
+      icon: "bi-bar-chart",
+      link: "https://www.youtube.com/watch?v=y72iyLPOv8E",
     },
     {
       title: "Introduction to Credit",
       description:
         "The ability to borrow dinero and understanding puntajes de crédito.",
       tagline: "Build your credit, build your futuro.",
+      icon: "bi-credit-card",
+      link: "https://www.youtube.com/watch?v=ozbGWLtZdoY",
     },
     {
       title: "Introduction to Retirement Planning",
       description: "Preparing financially for your años dorados.",
       tagline: "Secure your golden years hoy.",
+      icon: "bi-brightness-alt-high",
+      link: "https://www.youtube.com/watch?v=KA66Wcp1QEc",
     },
     {
       title: "Introduction to Taxes",
       description:
         "Understanding how ingresos is taxed and filing requirements.",
       tagline: "Be tax-savvy, keep more of your dinero.",
+      icon: "bi-paperclip",
+      link: "https://www.youtube.com/watch?v=6cRg9bnSnvg",
     },
     {
       title: "Introduction to Insurance",
       description: "Protecting yourself and activos from unexpected results.",
       tagline: "Protect what matters most to you.",
+      icon: "bi-clipboard-heart",
+      link: "https://www.youtube.com/watch?v=Gx0kHOibwHA",
     },
     {
       title: "Introduction to Financial Scams",
       description: "Recognizing and avoiding various types of fraudes.",
       tagline: "Stay vigilant, safeguard your finanzas.",
+      icon: "bi-binoculars",
+      link: "https://www.youtube.com/watch?v=7i6mPvBv488",
     },
     {
       title: "Introduction to Estate Planning",
       description:
         "Preparing for the distribution of your activos after death.",
       tagline: "Plan ahead, leave a legado.",
+      icon: "bi-clock",
+      link: "https://www.youtube.com/watch?v=cMoaGEpffds",
     },
   ];
 
@@ -76,6 +96,8 @@ const App = () => {
             title={card.title}
             description={card.description}
             tagline={card.tagline}
+            icon = {card.icon}
+            link = {card.link}
           />
         ))}
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({title, description, tagline}) => {
-    return (
-      <div className="card">
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <p>
-          <em>{tagline}</em>
-        </p>
-      </div>
-    );
+const Card = ({ title, description, tagline, icon, link}) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <i className={`bi ${icon}`}></i>
+      <p>
+        <em>{tagline}</em>
+      </p>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <button className="btn">Watch Video</button>
+      </a>
+    </div>
+  );
 };
+
 export default Card;
